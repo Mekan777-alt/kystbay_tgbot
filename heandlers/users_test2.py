9820144d164bca75419435d27bb05cefbc8bc3af
Mekan777-alt <mekan2678@gmail.com>
2023-04-22 02:26:09 +0300

    done test2

--- heandlers/users_test2.py
+++ heandlers/users_test2.py
@@ -18,7 +18,10 @@ async def tugan(message: types.Message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add('2. Тамле')
     photo = open('/home/mekan_bot/kystbay_tgbot/kst_data/tugan.JPG', 'rb')
+    video = 'BAACAgIAAxkBAAIRS2RDGL_ZfWjk7ub7RybNIevbiBQVAAJbLQACXmgZSn1bK1Pj2VUlLwQ'
+    await bot.send_chat_action(message.chat.id, ChatActions.UPLOAD_VIDEO)
     await bot.send_photo(message.chat.id, photo=photo, reply_markup=markup)
+    await bot.send_video(chat_id=message.chat.id, video=video)
 
 
 @dp.message_handler(text='2. Тамле')
@@ -26,7 +29,10 @@ async def tamle(message: types.Message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add('3. Абый')
     photo = open('/home/mekan_bot/kystbay_tgbot/kst_data/tamle.JPG', 'rb')
+    video = 'BAACAgIAAxkBAAIRTGRDGYsb6EQFMBBdZ1x_9pqbcyk_AAJdLQACXmgZSn3dvoSEBbyVLwQ'
+    await bot.send_chat_action(message.chat.id, ChatActions.UPLOAD_VIDEO)
     await bot.send_photo(message.chat.id, photo=photo, reply_markup=markup)
+    await bot.send_video(chat_id=message.chat.id, video=video)
 
 
 @dp.message_handler(text='3. Абый')
@@ -34,7 +40,10 @@ async def aby(message: types.Message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add('4. Гомбэ')
     photo = open('/home/mekan_bot/kystbay_tgbot/kst_data/aby.PNG', 'rb')
+    video = 'BAACAgIAAxkBAAIRTWRDGixGV1QSjYXrVDg9byWD9jf5AAJeLQACXmgZShb-5Qr6fBCCLwQ'
+    await bot.send_chat_action(message.chat.id, ChatActions.UPLOAD_VIDEO)
     await bot.send_photo(message.chat.id, photo=photo, reply_markup=markup)
+    await bot.send_video(chat_id=message.chat.id, video=video)
 
 
 @dp.message_handler(text='4. Гомбэ')
@@ -42,7 +51,10 @@ async def gombe(message: types.Message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add('5. Апа')
     photo = open('/home/mekan_bot/kystbay_tgbot/kst_data/gombe.JPG', 'rb')
+    video = 'BAACAgIAAxkBAAIRTmRDGpFwdCC4ov05MvYFvbIBzHRxAAJfLQACXmgZSvSiUaV_3cdzLwQ'
+    await bot.send_chat_action(message.chat.id, ChatActions.UPLOAD_VIDEO)
     await bot.send_photo(message.chat.id, photo=photo, reply_markup=markup)
+    await bot.send_video(chat_id=message.chat.id, video=video)
 
 
 @dp.message_handler(text='5. Апа')
@@ -50,7 +62,10 @@ async def apa(message: types.Message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add('6. Ак барс')
     photo = open('/home/mekan_bot/kystbay_tgbot/kst_data/apa.JPG', 'rb')
+    video = 'BAACAgIAAxkBAAIRT2RDGyNwGDeVo4IVFEaJbS_4K1NFAAJgLQACXmgZSlWxx704NSOTLwQ'
+    await bot.send_chat_action(message.chat.id, ChatActions.UPLOAD_VIDEO)
     await bot.send_photo(message.chat.id, photo=photo, reply_markup=markup)
+    await bot.send_video(chat_id=message.chat.id, video=video)
 
 
 @dp.message_handler(text='6. Ак барс')
@@ -58,4 +73,7 @@ async def ak_bars(message: types.Message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add('Начать третье упражнение')
     photo = open('/home/mekan_bot/kystbay_tgbot/kst_data/photo_2023-04-10 13.37.27.jpeg', 'rb')
+    video = 'BAACAgIAAxkBAAIRUGRDG3nI847qHVPiEODa_NImTTqLAAJhLQACXmgZSkDg7B6kOqyLLwQ'
+    await bot.send_chat_action(message.chat.id, ChatActions.UPLOAD_VIDEO)
     await bot.send_photo(message.chat.id, photo=photo, reply_markup=markup)
+    await bot.send_video(chat_id=message.chat.id, video=video)
