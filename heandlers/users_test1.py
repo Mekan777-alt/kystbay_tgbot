78632ecc105895d5bad8fb98c105c8d8055f874f
Mekan777-alt <mekan2678@gmail.com>
2023-05-02 15:53:18 +0300

    test rename education

--- heandlers/users_test1.py
+++ heandlers/users_test1.py
@@ -46,7 +46,7 @@ async def start_education(message: types.Message):
                          'Приступим к первому уроку. Я предоставлю тебе материалы в PDF формате и видеороликах.\n'
                          'Для закрепления информации ты будешь проходить тесты.')
     await message.answer("Начнем с первой темы 'Внешний вид на рабочем месте'", reply_markup=markup)
-    video = open('/home/mekan_bot/kystbay_tgbot/kst_data/vneshniyvid.MP4', 'rb')
+    video = open('/root/bot/kystbay_tgbot/kst_data/vneshniyvid.MP4', 'rb')
     doc = open('/root/bot/kystbay_tgbot/kst_data/forma.pptx', 'rb')
     await bot.send_chat_action(message.chat.id, ChatActions.UPLOAD_VIDEO)
     await bot.send_video(chat_id=message.chat.id, video=video)
